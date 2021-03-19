0cfe1b85cfc570d50744ef4887696a53b3be5553
Machalka <al1999dk@gmail.com>
2021-03-19 22:23:50 +0300

    правки

---
+++ Shop/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Shop(models.Model):
+    user=models.ForeignKey("User.User", unique=True, blank=False, null=False, on_delete=models.CASCADE, verbose_name="Пользователь")
+    name=models.CharField(max_length=50, blank=False, null=False, verbose_name="Название магазина")
+    description=models.CharField(max_length=250, blank=True, null=False, default="", verbose_name="Описание")
+    url=models.URLField(blank=False, null=False, blank=False, verbose_name="Ссылка")
+    logo=models.ImageField(upload_to="logo/", null=True, blank=True, verbose_name="Логотип")
+    def __str__(self):
+        self.name
+    class Meta:
+        verbose_name = 'Магазин'
+        verbose_name_plural = 'Магазины'
+
+class Goods(models.Model):
+    shop=models.ForeignKey(Shop, blank=False, null=False, verbose_name="Магазин")
+    stream=models.ForeignKey("Stream.Stream", blank=False, null=False, verbose_name="Стрим")
+    name=models.CharField(max_length=50, blank=False, null=False, verbose_name="Название товара")
+    description=models.CharField(max_length=250, blank=True, null=False, default="", verbose_name="Описание")
+    cost=models.PositiveIntegerField(blank=False, null=False, verbose_name="Стоимость")
+    url=models.URLField(blank=False, null=False, blank=False, verbose_name="Ссылка")
+    def __str__(self):
+        self.name
+    class class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+
